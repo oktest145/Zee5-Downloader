2cdbe81789446c9250f6f003fffb7b3c7acaabe3
oktest145 <83488344+oktest145@users.noreply.github.com>
2021-05-07 13:10:50 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,15 +3,15 @@ import os
 class Config(object):
 
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1766465768:AAHD3oKBm_Qkkb6RMTXMQxkZPyjwdlnHzfo")
 
     # The Telegram API things
     # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
+    APP_ID = int(os.environ.get("APP_ID", 3900425))
+    API_HASH = os.environ.get("API_HASH", "1affe5d43671e10d86514330afe05028")
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "-1001136335119").split())
 
     # Banned Unwanted Members..
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
@@ -22,11 +22,11 @@ class Config(object):
     # Telegram maximum file upload size
     TG_MAX_FILE_SIZE = 2097152000
 
-    # chunk size that should be used with requests
+    # chunk size that should
     CHUNK_SIZE = 128
 
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/0b57cc14e322dbc4998f9.jpg")
     
     # Sql Database url
     DB_URI = os.environ.get("DATABASE_URL", "")
